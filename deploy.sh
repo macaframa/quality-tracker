3c9dbf8d2312f74d4216e0f31a4399b6c68cd8d8
macaframa <projects.daniel.lopez@gmail.com>
2023-05-13 13:11:53 -0700

    circle ci yaml

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
-mkdir -p deployments/$timestamp
-cp bundle.zip deployments/$timestamp
-cd deployments/$timestamp
-unzip bundle.zip
-npm install
-npm run build
-pm2 del app
-pm2 start 'npm start' --name app
